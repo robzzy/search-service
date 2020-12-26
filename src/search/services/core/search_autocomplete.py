25b11bab457673777de1f8d4ee37fe98d6c69fe1
zzy <15779712170@163.com>
2020-12-26 20:25:56 +0800

    get_autocomplte only return result

--- src/search/services/core/search_autocomplete.py
+++ src/search/services/core/search_autocomplete.py
@@ -4,11 +4,12 @@ from nameko.rpc import rpc
 
 class AutocompleteMixin:
     @rpc
-    def search_autocomplete(self, **search_params):
+    def get_autocomplete(self, **search_params):
         hits = self.searcher("autocomplete-v1", **search_params).to_dict()["hits"][
             "hits"
         ]
 
         response = [hit["_source"] for hit in hits]
 
-        return response
+        result = [i["autocomplete"] for i in response]
+        return result
